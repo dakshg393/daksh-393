ce71716908a1ce1b4fe217168f040b0e5d1c47b6
dakshg393 <142725301+dakshg393@users.noreply.github.com>
2024-11-23 12:50:39 +0530

    Edit About

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             </div>
             <div id='about-main' className="h-full flex flex-col md:flex-row bg-mainBg">
                 <div className='h-1/2 md:h-full md:w-1/2'>
-                    <p className='text-center text-secondaryText p-4 font-fontParagraph'>I'm a passionate MERN stack developer currently in my 3rd year of Bachelor of Computer Applications (BCA). I thrive on building interactive, responsive, and efficient web applications that solve real-world problems. With a strong foundation in MongoDB, Express, React, and Node.js, I am continuously exploring new challenges and pushing my skills further in full-stack development.
+                    <p className='text-center text-secondaryText p-4 font-fontParagraph'>I'm Daksh Gupta a passionate MERN stack developer currently in my 3rd year of Bachelor of Computer Applications (BCA). I thrive on building interactive, responsive, and efficient web applications that solve real-world problems. With a strong foundation in MongoDB, Express, React, and Node.js, I am continuously exploring new challenges and pushing my skills further in full-stack development.
 
                         I enjoy crafting projects that go beyond standard functionality, integrating automation, APIs, and scalable solutions. Whether it’s creating intuitive user experiences on the frontend or implementing secure, reliable servers on the backend, I strive to deliver quality, user-focused applications.
                         Here are some of the key skills I bring to my projects:
